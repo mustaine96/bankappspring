d1c7c78b891e6840476ac13b4a61aff1b0586033
vipagraw <vipagraw@IN-MUM-LDVM030.corp.capgemini.com>
2018-09-19 08:22:52 -0400

    Spring first commit

---
+++ src/main/java/com/capgemini/bankapp/service/BankAccountService.java
@@ -0,0 +1,13 @@
+package com.capgemini.bankapp.service;
+
+import com.capgemini.bankapp.service.impl.LowBalanceException;
+
+public interface BankAccountService {
+	
+	public double getBalance(long accountId);
+	public double withdraw(long accountId, double amount);
+	public double deopsit(long accountId, double amount);
+	public boolean fundTransfer(long fromAccount, long toAccount, double balance)throws LowBalanceException;;
+	double deposit(long accountId, double amount);
+
+}
---
+++ src/main/java/com/capgemini/bankapp/service/impl/LowBalanceException.java
@@ -0,0 +1,6 @@
+package com.capgemini.bankapp.service.impl;
+
+public class LowBalanceException extends Exception {
+	
+
+}
